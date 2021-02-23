88211d87169a368aca782551a34b183e4e4b64fe
Jason Slade <jason.slade906@gmail.com>
2021-02-23 14:56:54 -0700

    get and * paths

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+
+    res.send('Hello from Express!')
+})
+
+app.all('*', (req, res)=> {
+    const random = Math.floor(Math.random() * Math.floor(10))
+    res.render('index', { path: req.path, method: req.method, random})
+})
+
+const port = 8081;
+app.listen(port, ()=> console.log(`Listening on: ${port}`));
