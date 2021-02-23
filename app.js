e050999fd2f4e2848842f5093c45e11a5e5cdd66
jeb griffin <jebgriffin@jebs-MacBook-Pro.local>
2021-02-23 16:13:14 -0600

    phs3

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ app.get('/', (req, res) => {
     res.send('Hello from Express!')
 })
 
+app.get(/^\/.*xyz\/?$/, (req, res) => {
+    res.send("That's all I wrote.")
+})
+
 app.all('*', (req, res)=> {
     const random = Math.floor(Math.random() * Math.floor(10))
     res.render('index', { path: req.path, method: req.method, random})
 })
+
 
 const port = 8081;
 app.listen(port, ()=> console.log(`Listening on: ${port}`));
